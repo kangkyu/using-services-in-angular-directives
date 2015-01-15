62118508af9d802c90d365b49b80379616bde513
kangkyu <kangkyu1111@gmail.com>
2015-01-15 10:26:09 -0800

    refactor by adding ng-controller

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 angular.module("app", ["ui.bootstrap"])
+.controller("AppCtrl", function AppCtrl(){
+    var app = this;
+
+    app.alertMessage = "Something Failed";
+    app.alertType = "danger";
+    app.showAlert = true;
+})
